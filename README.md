68088f89aa866c870267b66072daafe79f1eb758
alekseyl <leshchuk@gmail.com>
2017-09-08 15:02:55 +0300

    read me, red replaced with colorize_err, blank? with nil?

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # Niceql
 
-This is small, nice, simple and dependentless solution for SQL prettifiyng. 
-It can be used in irb console without any dependencies.
+This is small, nice, simple and dependentless solution for SQL prettifiyng for Ruby. 
+It can be used in irb console without any dependencies ( run ./console from bin and look for examples ).
+
+Any reasonable suggestions on formatting/coloring are welcome
 
 ## Before/After 
-###SQL prettifier: 
+### SQL prettifier: 
 ![alt text](https://github.com/alekseyl/niceql/raw/master/to_niceql.png "To_niceql")
 
-###PG errors prettifier 
+### PG errors prettifier 
+
 before: 
 ![alt text](https://github.com/alekseyl/niceql/raw/master/err_was.png "To_niceql")
 
 after:
 ![alt text](https://github.com/alekseyl/niceql/raw/master/err_now.png "To_niceql")
+
 
 ## Installation
 
@@ -32,7 +36,56 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+### With ActiveRecord
+
+```ruby
+  # prettify to_sql 
+  Model.scope.to_niceql
+  
+  # prettify PG errors 
+  Model.scope_with_err.explain_err 
+```
+
+### Without ActiveRecord
+
+```ruby
+   puts Niceql::Prettifier.prettify_sql("SELECT * FROM ( VALUES(1), (2) ) AS tmp")
+   
+   # see colors in irb %) 
+   #=>  SELECT * 
+   #=>  FROM ( VALUES(1), (2) ) AS tmp
+
+   # rails combines err with query, so don't forget to do it yourself 
+   # to get real nice result you should execute on your DB prettified_sql! 
+   # otherwise you will not get such a nice output
+   
+   puts Niceql::Prettifier.prettify_pg_err(<<-ERR )
+ERROR:  VALUES in FROM must have an alias
+LINE 2:  FROM ( VALUES(1), (2) )
+              ^
+HINT:  For example, FROM (VALUES ...) [AS] foo.
+ SELECT err 
+ FROM ( VALUES(1), (2) )
+ ORDER BY 1
+ERR
+   
+
+# ERROR:  VALUES in FROM must have an alias
+# LINE 2:  FROM ( VALUES(1), (2) )
+#               ^
+#     HINT:  For example, FROM (VALUES ...) [AS] foo.
+#     SELECT err
+#     FROM ( VALUES(1), (2) )
+#          ^
+#     ORDER BY 1
+
+```
+## Limitations
+
+Right now it detect only uppercase verbs with simple indentation and parsing options. 
+Also if your console support more colors or different schemes, or you prefer different colorization you can override ColorizeString 
+methods. Current color are selected with dark and white console themes in mind, so it works good for dark, and good enough for white.
+
 
 ## Development
 
--- lib/niceql.rb
+++ lib/niceql.rb
@@ -10,6 +10,10 @@ module Niceql
       #cyan ANSI color
       "\e[0;36;49m#{str}\e[0m"
     end
+    def self.colorize_err(err)
+      #red ANSI color
+      "\e[0;31;49m#{err}\e[0m"
+    end
   end
 
   module ArExtentions
@@ -27,8 +31,8 @@ module Niceql
   end
 
   module Prettifier
-    INLINE_VERBS = 'ASC| IN|AS|WHEN|THEN|ELSE|END|AND|UNION|ALL'
-    NEW_LINE_VERBS = 'SELECT|FROM|WHERE|CASE|ORDER BY|LIMIT|GROUP BY'
+    INLINE_VERBS = %w(ASC IN AS WHEN THEN ELSE END AND UNION ALL WITH ON DISTINCT INTERSECT EXCEPT EXISTS NOT).join('| ')
+    NEW_LINE_VERBS = 'SELECT|FROM|WHERE|CASE|ORDER BY|LIMIT|GROUP BY|WITH|LEFT JOIN|RIGHT JOIN|JOIN|HAVING|OFFSET'
     VERBS = "#{INLINE_VERBS}|#{NEW_LINE_VERBS}"
     STRINGS = /("[^"]+")|('[^']+')/
     BRACKETS = '[\(\)]'
@@ -41,18 +45,19 @@ module Niceql
       end
     end
 
+
     def self.prettify_pg_err(err)
       err_line_num = err[/LINE \d+/][5..-1].to_i
       start_sql_line = err.lines[3][/HINT/] ? 4 : 3
       err_body = err.lines[start_sql_line..-1]
-      err_line = err_body[err_line_num-1].red
+      err_line = StringColorize.colorize_err( err_body[err_line_num-1] )
 
       err_body = err_body.join.gsub(/#{VERBS}/ ) { |verb| StringColorize.colorize_verb(verb) }
       err_body = err_body.gsub(STRINGS){ |str| StringColorize.colorize_str(str) }
 
       err_body = err_body.lines
       err_body[err_line_num-1]= err_line
-      err_body.insert( err_line_num, err.lines[2][err.lines[1][/LINE \d+:/].length+1..-1].red )
+      err_body.insert( err_line_num, StringColorize.colorize_err( err.lines[2][err.lines[1][/LINE \d+:/].length+1..-1] ) )
       puts err.lines[0..start_sql_line-1].join + err_body.join
     end
 
@@ -73,16 +78,16 @@ module Niceql
           indent += 1
         elsif verb == ')'
           # this also covers case when right bracket is used without corresponding left one
-          add_new_line = parentness.last.blank? || parentness.last[:nested]
+          add_new_line = parentness.last.nil? || parentness.last[:nested]
           indent -= add_new_line ? 2 : 1
           indent = 0 if indent < 0
           parentness.pop
         elsif verb == 'ORDER BY'
           # in postgres ORDER BY can be used in aggregation function this will keep it
           # inline with its agg function
-          add_new_line = parentness.last.blank? || parentness.last[:nested]
+          add_new_line = parentness.last.nil? || parentness.last[:nested]
         else
-          add_new_line = verb[/(#{INLINE_VERBS})/].blank?
+          add_new_line = verb[/(#{INLINE_VERBS})/].nil?
         end
         verb = StringColorize.colorize_verb(verb) if !['(', ')'].include?(verb) && colorize
 
